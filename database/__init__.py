4334c75a4e43ec85437cf8fa1e06c27c6a2eaa80
TTW <s104321072@mail1.ncnu.edu.tw>
2019-06-17 23:28:20 +0800

    fix(remove sensitive content):

--- database/__init__.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-db = pymysql.connect(host='140.116.163.9',
-                     port=10002,
-                     user='dltlab',
-                     password='dltlab@nckucs',
-                     db='epslab_ems',
-                     charset='utf8')
